99fab7109366b9d9c105931823d44ae3347a71b1
Josua <josua.ramouche@live.fr>
2023-10-10 22:52:12 +0200

    Dependency for database added and verified + Failed connection tentative

--- src/main/java/Connect.java
+++ src/main/java/Connect.java
@@ -1,9 +1,26 @@
+import java.sql.*;
 
-public class Connect {
+public class Connect{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args){
 
-	}
+    //mdp : Ohv3uz6x   groupe 31 
+    //Nom de la base de données : projet_gei_31
+    //Le login d'accès à la base de donnée : projet_gei_31
+    String url = "jdbc:mysql://srv-bdens.insa-toulouse.fr:3306/projet_gei_31";
+    String user = "projet_gei_31";
+    String password = "Ohv3uz6x";
+
+        try {
+            Connection con = DriverManager.getConnection(url,user,password);
+            System.out.println("Connected successfully\n");
+        }
+        catch (SQLException e)
+        {
+            System.out.println("Connection failed " + e.getMessage() + "\n");
+            e.printStackTrace();
+        }
+
+    }
 
 }
