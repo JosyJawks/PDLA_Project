4e7a85709d1ff729d73c9a722c7ed6ec1618c161
SaladeVerteee <cossoul.lucile@gmail.com>
2023-10-13 14:21:35 +0200

    Conflict

--- src/main/java/Connect.java
+++ src/main/java/Connect.java
@@ -4,24 +4,9 @@ public class Connect{
 
     public static void main(String[] args){
 
-<<<<<<< HEAD
-    //mdp : Ohv3uz6x   groupe 31 
-    //Nom de la base de données : projet_gei_31
-    //Le login d'accès à la base de donnée : projet_gei_31
-    String url = "jdbc:mysql://srv-bdens.insa-toulouse.fr:3306/projet_gei_31";
-    String user = "projet_gei_31";
-    String password = "Ohv3uz6x";
 
-        try {
-            Connection con = DriverManager.getConnection(url,user,password);
-            System.out.println("Connected successfully\n");
-        }
-        catch (SQLException e)
-        {
-            System.out.println("Connection failed " + e.getMessage() + "\n");
-            e.printStackTrace();
-        }//test2
-=======
+    
+
     	Connection con = null;
 	    //mdp : Ohv3uz6x  
 	    //Nom de la base de données : projet_gei_31
@@ -39,7 +24,7 @@ public class Connect{
 	            System.out.println("Connection failed " + e.getMessage() + "\n");
 	            e.printStackTrace();
 	        }
->>>>>>> branch 'main' of https://github.com/JosyJawks/PDLA_Project.git
+
 
     }
 
