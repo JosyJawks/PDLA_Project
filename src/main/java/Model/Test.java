fd9552fce810f9eb7d9743aebd0043906f1dd706
Josua <josua.ramouche@live.fr>
2023-10-16 15:27:48 +0200

    UserController

--- src/main/java/Model/Test.java
+++ src/main/java/Model/Test.java
@@ -6,8 +6,7 @@ public class Test {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		Client c1 = new Client("Josua","Ramouche","ramouche@insa-toulouse.fr");
-		//c1.addMission();
-		Database.Connect();
+		c1.addMission();
 	}
 
 }
