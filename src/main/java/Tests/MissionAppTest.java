1537dd2a5c394882a45633ddc5eec21c6b452280
SaladeVerteee <cossoul.lucile@gmail.com>
2023-11-03 17:23:21 +0100

    Tests package JUnit

---
+++ src/main/java/Tests/MissionAppTest.java
@@ -0,0 +1,14 @@
+package Tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MissionAppTest {
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getFinal() {
+    }
+}
